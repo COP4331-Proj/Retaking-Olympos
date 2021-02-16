864d1db852d604b82c1ba2280535bbe32edb033e
Gokamo <48892704+Gokamo@users.noreply.github.com>
2021-02-16 14:00:42 -0500

    Created View Gladiator Scene file and Gladiator class

---
+++ project/Retaking Olympos/Assets/Scripts/Gladiator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gladiator
+{
+    private string gladiatorName;
+    private int level;
+    private int health;
+    private int stamina;
+    private int power;
+    private int defense;
+
+    public Gladiator(string name, int level, int health, int stamina, int power, int defense) 
+    {
+        this.gladiatorName = name;
+        this.level = level;
+        this.health = health;
+        this.stamina = stamina;
+        this.power = power;
+        this.defense = defense;
+    }
+
+    // Getter and setters for gladiator stats
+    public string GetName() 
+    {
+        return this.gladiatorName;
+    }
+    public void SetName(string name)
+    {
+        this.gladiatorName = name;
+    }
+
+    public int GetLevel()
+    {
+        return this.level;
+    }
+    public void SetLevel(int level)
+    {
+        this.level = level;
+    }
+    public int GetHealth()
+    {
+        return this.health;
+    }
+    public void SetHealth(int health)
+    {
+        this.health = health;
+    }
+    public int GetStamina()
+    {
+        return this.stamina;
+    }
+    public void SetStamina(int stamina)
+    {
+        this.stamina = stamina;
+    }
+    public int GetPower()
+    {
+        return this.power;
+    }
+    public void SetPower(int power)
+    {
+        this.power = power;
+    }
+    public int GetDefense()
+    {
+        return this.defense;
+    }
+    public void SetDefense(int defense)
+    {
+        this.defense = defense;
+    }
+}
---
+++ project/Retaking Olympos/Assets/Scripts/GladiatorBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GladiatorBehavior : MonoBehaviour
+{
+    Gladiator gladiator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gladiator = new Gladiator("Caesar", 3, 100, 100, 7, 16);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
