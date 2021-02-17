60780fe543cf528d1cf8bf6d7143eab820ec8620
NotTsunami <4589807+NotTsunami@users.noreply.github.com>
2021-02-17 12:14:57 -0500

    Volume option implemented
    - Connect title BGM to mixer
    - Load/Save VolumePreference to PlayerPrefs

--- Retaking Olympos/Assets/Scripts/SettingsMenu.cs
+++ Retaking Olympos/Assets/Scripts/SettingsMenu.cs
@@ -2,13 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider volumeSlider;
+    float currentVolume;
+
+    public void Awake()
+    {
+        LoadSettings();
+    }
+
+    public void OnDestroy()
+    {
+        SaveSettings();
+    }
 
     public void setVolume (float volume)
     {
         audioMixer.SetFloat("Volume", volume);
+        currentVolume = volume;
+    }
+
+    public void LoadSettings()
+    {
+        if (PlayerPrefs.HasKey("VolumePreference"))
+        {
+            volumeSlider.value = PlayerPrefs.GetFloat("VolumePreference");
+        }
+		
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetFloat("VolumePreference", currentVolume);
+
     }
 }
