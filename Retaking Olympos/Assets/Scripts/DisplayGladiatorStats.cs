020f5e655a485e973a292973937516b7c7473c4d
Gokamo <48892704+Gokamo@users.noreply.github.com>
2021-02-20 17:43:13 -0500

    Linked DisplayGladiatorStats to the UI elements on ViewGladiator Scene
    Note: The view gladiator script has an misleading or at least not good name, id like to change it but ill want to make sure that doesn't mess anyone up first

---
+++ Retaking Olympos/Assets/Scripts/DisplayGladiatorStats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayGladiatorStats : MonoBehaviour
+{
+    List<Gladiator> gladiatorList;
+    int gladiatorIndex = 0;
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI levelText;
+    [SerializeField] TextMeshProUGUI healthText;
+    [SerializeField] TextMeshProUGUI staminaText;
+    [SerializeField] TextMeshProUGUI powerText;
+    [SerializeField] TextMeshProUGUI defenseText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ViewGladiator viewGladiator = new ViewGladiator();
+        viewGladiator.createNewGladiator("Caesar", 3, 100, 100, 6, 14);
+        viewGladiator.createNewGladiator("Bob", 3, 100, 100, 6, 14);
+        gladiatorList = viewGladiator.GetGladiatorList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RefreshStats();
+    }
+
+    public void RefreshStats() 
+    {
+        nameText.text = gladiatorList[gladiatorIndex].GetName().ToString();
+        levelText.text = gladiatorList[gladiatorIndex].GetLevel().ToString();
+        healthText.text = gladiatorList[gladiatorIndex].GetHealth().ToString();
+        staminaText.text = gladiatorList[gladiatorIndex].GetStamina().ToString();
+        powerText.text = gladiatorList[gladiatorIndex].GetPower().ToString();
+        defenseText.text = gladiatorList[gladiatorIndex].GetDefense().ToString();
+    }
+
+}
--- Retaking Olympos/Assets/Scripts/ViewGladiator.cs
+++ Retaking Olympos/Assets/Scripts/ViewGladiator.cs
@@ -2,26 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ViewGladiator : MonoBehaviour
+public class ViewGladiator
 {
     List<Gladiator> gladiatorList = new List<Gladiator>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        createNewGladiator("Caesar", 3, 100, 100, 6, 14);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Debug.Log(gladiatorList[0].GetHealth());
-    }
-
-    void createNewGladiator(string name, int level, int health, int stamina, int power, int defense)
+    public void createNewGladiator(string name, int level, int health, int stamina, int power, int defense)
     {
         Gladiator gladiator;
         gladiator = new Gladiator(name, level, health, stamina, power, defense);
         gladiatorList.Add(gladiator);
+    }
+
+    public List<Gladiator> GetGladiatorList() 
+    {
+        return gladiatorList;
     }
 }
