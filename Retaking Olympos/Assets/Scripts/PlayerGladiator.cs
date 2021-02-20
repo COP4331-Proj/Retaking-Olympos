4e31801f4181a28259484bf98e3b9ae063a48423
Xavier Huetter <xjhuetter@yahoo.com>
2021-02-20 17:05:34 -0500

    Added Functional Stat Bars for Battle
    Added health and stamina bar for player character in battle stage

---
+++ Retaking Olympos/Assets/Scripts/PlayerGladiator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGladiator : MonoBehaviour
+{
+    public int currentHealth;
+    public int currentStamina;
+    public Gladiator player = new Gladiator("Player", 1, 100, 100, 6, 14);
+    
+    public HealthBar healthBar;
+    public StaminaBar staminaBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = player.GetHealth();
+        healthBar.setMaxHealth(player.GetHealth());
+        currentStamina = player.GetStamina();
+        staminaBar.setMaxStamina(player.GetStamina());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            takeDamage(10);
+            useSkill(10);
+        }
+    }
+
+    void takeDamage(int damage)
+    {
+        if (currentHealth <= 0)
+            return;
+
+        currentHealth -= damage;
+
+        healthBar.setHealth(currentHealth);
+    }
+
+    void useSkill(int stamina)
+    {
+        if (currentStamina <= 0)
+            return;
+
+        currentStamina -= stamina;
+
+        staminaBar.setStamina(currentStamina);
+    }
+}
