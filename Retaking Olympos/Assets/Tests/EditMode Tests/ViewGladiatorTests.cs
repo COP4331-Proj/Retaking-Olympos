4fc66f5f7aa0d3bc1ef2a7046931aa75b8c4da87
Gokamo <48892704+Gokamo@users.noreply.github.com>
2021-02-20 20:50:12 -0500

    Updated ViewGladiatorTests

--- Retaking Olympos/Assets/Tests/EditMode Tests/ViewGladiatorTests.cs
+++ Retaking Olympos/Assets/Tests/EditMode Tests/ViewGladiatorTests.cs
@@ -8,6 +8,7 @@ namespace Tests
 {
     public class ViewGladiatorTests
     {
+        
         [Test]
         public void CreateGladiatorObject()
         {
@@ -16,5 +17,64 @@ namespace Tests
             Assert.IsNotNull(gladiator);
         }
 
+
+        [Test]
+        public void TestGladiatorName() 
+        {
+            ViewGladiator viewGladiator = new ViewGladiator();
+            Gladiator gladiator = viewGladiator.createNewGladiator("Caesar", 3, 100, 100, 6, 14);
+            Assert.AreEqual(gladiator.GetName(), "Caesar");
+        }
+
+        [Test]
+        public void TestGladiatorLevel()
+        {
+            ViewGladiator viewGladiator = new ViewGladiator();
+            Gladiator gladiator = viewGladiator.createNewGladiator("Caesar", 3, 100, 100, 6, 14);
+            Assert.AreEqual(gladiator.GetLevel(), 3);
+        }
+
+        [Test]
+        public void TestGladiatorHealth()
+        {
+            ViewGladiator viewGladiator = new ViewGladiator();
+            Gladiator gladiator = viewGladiator.createNewGladiator("Caesar", 3, 100, 100, 6, 14);
+            Assert.AreEqual(gladiator.GetHealth(), 100);
+        }
+
+        [Test]
+        public void TestGladiatorStamina()
+        {
+            ViewGladiator viewGladiator = new ViewGladiator();
+            Gladiator gladiator = viewGladiator.createNewGladiator("Caesar", 3, 100, 100, 6, 14);
+            Assert.AreEqual(gladiator.GetStamina(), 100);
+        }
+
+        [Test]
+        public void TestGladiatorPower()
+        {
+            ViewGladiator viewGladiator = new ViewGladiator();
+            Gladiator gladiator = viewGladiator.createNewGladiator("Caesar", 3, 100, 100, 6, 14);
+            Assert.AreEqual(gladiator.GetPower(), 6);
+        }
+
+        [Test]
+        public void TestGladiatorDefense()
+        {
+            ViewGladiator viewGladiator = new ViewGladiator();
+            Gladiator gladiator = viewGladiator.createNewGladiator("Caesar", 3, 100, 100, 6, 14);
+            Assert.AreEqual(gladiator.GetDefense(), 14);
+        }
+
+        [Test]
+        public void TestAddGladiatorToList() 
+        {
+            ViewGladiator viewGladiator = new ViewGladiator();
+            List<Gladiator> gladiatorList = viewGladiator.GetGladiatorList();
+            Assert.AreEqual(0, gladiatorList.Count);
+            viewGladiator.createNewGladiator("Caesar", 3, 100, 100, 6, 14);
+            viewGladiator.createNewGladiator("Bob", 4, 120, 120, 10, 12);
+            Assert.AreEqual(2, gladiatorList.Count);
+        }
     }
 }
