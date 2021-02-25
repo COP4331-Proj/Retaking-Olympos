c55c95bbda4c4f9d425e39af9890d98fe3fe9722
Olumnem <gnot.yhtomit@gmail.com>
2021-02-25 15:32:43 -0500

    added mute button to options menu

---
+++ Retaking Olympos/Assets/Scripts/MuteManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteManager : MonoBehaviour
+{
+    private bool isMuted = false;
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("muted"))
+            PlayerPrefs.SetInt("muted", 0);
+        else
+            loadMuteState();
+    }
+
+    public void mutePressed()
+    {
+        isMuted = !isMuted;
+        AudioListener.pause = isMuted;
+        saveMuteState();
+        updateText();
+    }
+
+    private void saveMuteState()
+    {
+        PlayerPrefs.SetInt("muted", isMuted ? 1 : 0);
+    }
+
+    private void loadMuteState()
+    {
+        isMuted = PlayerPrefs.GetInt("muted") == 1;
+        updateText();
+    }
+
+    private void updateText()
+    {
+        Text muteStatus = GameObject.Find("Canvas/Mute/Text").GetComponent<Text>();
+        if (isMuted)
+            muteStatus.text = "Unmute";
+        else
+            muteStatus.text = "Mute";
+    }
+}
