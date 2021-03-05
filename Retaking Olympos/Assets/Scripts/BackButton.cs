b45d37c1e6c4c2498931d700c4e0a74d0129ef47
NotTsunami <4589807+NotTsunami@users.noreply.github.com>
2021-03-04 20:36:37 -0500

    Add a general back button to the main scene

---
+++ Retaking Olympos/Assets/Scripts/BackButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour
+{
+    private static String currentClass;
+
+    public void GoBack()
+    {
+        GameObject gameObject = new GameObject();
+        gameObject.AddComponent<SceneLoader>();
+        currentClass = SceneManager.GetActiveScene().name;
+
+        if (currentClass == "Shop")
+        {
+            gameObject.GetComponent<SceneLoader>().GoToScene("View Gladiator");
+        } 
+        else if (currentClass == "Main Scene")
+        {
+            gameObject.GetComponent<SceneLoader>().GoToScene("Title Screen");
+        }
+    }
+}
