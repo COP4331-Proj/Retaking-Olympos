c9c03f27020174b2474c5aab6a44cd2943a2f1c9
Olumnem <gnot.yhtomit@gmail.com>
2021-03-11 18:50:07 -0500

    Introduced a basic save script for the player gladiator(still needs a massive amount of work), fixed some comments in the gladiator classes test script

---
+++ Retaking Olympos/Assets/Scripts/SaveingLoading/SaveManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveManager
+{
+    public static void SavePlayerData(PlayerGladiator player)
+    {
+        string path = Application.persistentDataPath + "/game.RO";
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(player);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayerData()
+    {
+        string path = Application.persistentDataPath + "/game.RO";
+
+        // Gaurds against missing save
+        if (!File.Exists(path))
+        {
+            Debug.LogError("Save file not found in: " + path);
+            return null;
+        }
+
+        BinaryFormatter formatter= new BinaryFormatter();
+        FileStream stream = new FileStream(path, FileMode.Open);
+        PlayerData data = formatter.Deserialize(stream) as PlayerData;
+        stream.Close();
+
+        return data;
+    }
+}
