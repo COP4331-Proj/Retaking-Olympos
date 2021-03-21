b6edb1c5d37216be1202b39ac4a56e94de6165c2
Gokamo <48892704+Gokamo@users.noreply.github.com>
2021-03-21 17:11:33 -0400

    Enemy gladiator faces player

--- Retaking Olympos/Assets/Scripts/EnemyMovement.cs
+++ Retaking Olympos/Assets/Scripts/EnemyMovement.cs
@@ -27,6 +27,17 @@ public class EnemyMovement : MonoBehaviour
     public void enemyChase()
     {
         transform.position = Vector2.MoveTowards(transform.position, playerChar.position, movementSpeed * Time.deltaTime);
+        //Debug.Log("Player" + playerChar.position.x + "enem" + transform.position.x);
+        
+        if (playerChar.position.x > transform.position.x)
+        {
+            Debug.Log("true");
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else 
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
     }
 
 
