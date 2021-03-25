5d077ade23c68e00b41813f89c87c2fc792d4ef7
jjclaybaugh <joshuaclaybaugh@gmail.com>
2021-03-25 19:46:15 -0400

    $$$

---
+++ Retaking Olympos/Assets/Scripts/MoneyMaker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyMaker : MonoBehaviour
+{
+    public HoldPlayerInformation player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.gold += 250;
+    }
+}
