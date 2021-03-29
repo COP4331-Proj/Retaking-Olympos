b8967821bb16e80ab4b3ac7daff0e5748e3375cd
Gokamo <48892704+Gokamo@users.noreply.github.com>
2021-03-28 23:31:32 -0400

    Added end game scene and charts

---
+++ Retaking Olympos/Assets/Scripts/Victory.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+    public GameObject sceneloader;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (PlayerPrefs.GetInt("VerusBeat") == 1)
+        {
+            if (PlayerPrefs.GetInt("TetraitesBeat") == 1)
+            {
+                if (PlayerPrefs.GetInt("SilvanusBeat") == 1)
+                {
+                    if (PlayerPrefs.GetInt("MarcusBeat") == 1)
+                    {
+                        if (PlayerPrefs.GetInt("CassiusBeat") == 1)
+                        {
+                            if (PlayerPrefs.GetInt("SpartacusBeat") == 1)
+                            {
+                                if (PlayerPrefs.GetInt("FlammaBeat") == 1)
+                                {
+                                    if (PlayerPrefs.GetInt("CrixusBeat") == 1)
+                                    {
+                                        if (PlayerPrefs.GetInt("CommodusBeat") == 1)
+                                        {
+                                            if (PlayerPrefs.GetInt("CarpophorusBeat") == 1)
+                                            {
+                                                if (sceneloader != null) 
+                                                {
+                                                    sceneloader.GetComponent<SceneLoader>().GoToScene("End Game");
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+}
